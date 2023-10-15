c28553b0988ff917c2e199b3c85cdc6d57ae9755
Likhit Kumar V P <84926428+Likhit-Kumar@users.noreply.github.com>
2023-10-15 18:25:43 +0530

    Add files via upload

---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+$dbServername = "127.0.0.1:3306";
+$dbUsername = "root";
+$dbPassword = "1234";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername,$dbPassword,$dbName);
+
+?>
